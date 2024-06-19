7ccecb736ff4d37bfe907d7f7122f3b04a10385b
KHOA0302 <khoai.t0302@gmail.com>
2024-06-19 20:17:49 +0700

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import Test from "./Pages/Test";
+
+test("CI/CD should be worked", () => {
+  render(<Test />);
+  const linkElement = screen.getByText("CI/CD");
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/Pages/Test/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Test() {
+  return <h1>CI/CD</h1>;
+}
+
+export default Test;
